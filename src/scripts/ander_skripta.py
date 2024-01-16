d717f0fc420d952ccb97baed20d2efd155ecec63
andrejjurman <90095622+andrejjurman@users.noreply.github.com>
2024-01-16 12:17:47 +0100

    Update ander_skripta.py

--- src/scripts/ander_skripta.py
+++ src/scripts/ander_skripta.py
@@ -19,39 +19,24 @@ from transforms3d.quaternions import mat2quat, quat2mat
 
 from objectiveFunPosition import objectiveFunPosition
 from optimizePointRotation import f_optimizePointOrientation
+
 class CallipenCalibration:
     
     
        
-    def __init__(self):
+    def __init__(self, N_num_of_data):
          
         self.CalipenCalibrationData_list = list()
-        self.CalipenCalibrationData_tocka1 = list()
-        self.CalipenCalibrationData_tocka2 = list()
-        self.CalipenCalibrationData_num_of_data = 0
-        self.CalipenCalibrationData_num_of_data_tocka1 = 0
-        self.CalipenCalibrationData_num_of_data_tocka2 = 0
 
-        self.N_num_of_data = 500
+        self.CalipenCalibrationData_num_of_data = 0
+  
+        self.N_num_of_data = N_num_of_data
         self.flag_recorded = False
-        self.csv_filename = "csv_data.csv"
         self.flag_calculatedTransformation = False
-        self.flag_tocka1 = False
-        self.flag_tocka2 = False
         self.T_calib = None
-        self.N_num_of_data_tocka = 50
     
  
         
-        
-    def save_list_to_csv(self, my_list, csv_filename):
-    # Open a CSV file in write mode
-        with open(self.csv_filename, 'w', newline='') as csvfile:
-            # Create a CSV writer object
-            csv_writer = csv.writer(csvfile)
-
-            # Write the list elements as a single row in the CSV file
-            csv_writer.writerow(my_list)
         
     def record_data_callback1(self, data : PoseStamped):
         pose = data.pose
@@ -78,58 +63,7 @@ class CallipenCalibration:
         elif pose != self.CalipenCalibrationData_list[-1]:
             self.CalipenCalibrationData_list.append(T)
             self.CalipenCalibrationData_num_of_data += 1
-        
-    def record_data_callback2(self, data : PoseStamped):
-        pose = data.pose
-        q1 = pose.orientation.x
-        q2 = pose.orientation.y
-        q3 = pose.orientation.z
-        q4 = pose.orientation.w
-        x = pose.position.x
-        y = pose.position.y
-        z = pose.position.z
-        R = quat2mat([q1, q2, q3, q4])
-        
-        T = np.zeros([4, 4])
-        T[:3, :3] = R
-        T[0, 3] = x
-        T[1, 3] = y
-        T[2, 3] = z
-        T[3, 3] = 1
-        
-        if len(self.CalipenCalibrationData_tocka1) == 0:
-            self.CalipenCalibrationData_tocka1.append(T)
-            self.CalipenCalibrationData_num_of_data_tocka1 += 1
-            
-        elif pose != self.CalipenCalibrationData_tocka1[-1]:
-            self.CalipenCalibrationData_tocka1.append(T)
-            self.CalipenCalibrationData_num_of_data_tocka1 += 1
-            
-    def record_data_callback3(self, data : PoseStamped):
-        pose = data.pose
-        q1 = pose.orientation.x
-        q2 = pose.orientation.y
-        q3 = pose.orientation.z
-        q4 = pose.orientation.w
-        x = pose.position.x
-        y = pose.position.y
-        z = pose.position.z
-        R = quat2mat([q1, q2, q3, q4])
-        
-        T = np.zeros([4, 4])
-        T[:3, :3] = R
-        T[0, 3] = x
-        T[1, 3] = y
-        T[2, 3] = z
-        T[3, 3] = 1
-        
-        if len(self.CalipenCalibrationData_tocka2) == 0:
-            self.CalipenCalibrationData_tocka2.append(T)
-            self.CalipenCalibrationData_num_of_data_tocka2 += 1
-            
-        elif pose != self.CalipenCalibrationData_tocka2[-1]:
-            self.CalipenCalibrationData_tocka2.append(T)
-            self.CalipenCalibrationData_num_of_data_tocka2 += 1        
+      
             
 
     def RecordData_calipen(self):
@@ -137,28 +71,10 @@ class CallipenCalibration:
         while True:
             if self.CalipenCalibrationData_num_of_data > self.N_num_of_data:
                 self.flag_recorded = True
-                self.save_list_to_csv(self.CalipenCalibrationData_list, self.csv_filename)
                 callipenpose_subscriber1.unregister()
                 break
-            
-    def RecordData_tocka1(self):
-        callipenpose_subscriber2 = rospy.Subscriber("/vrpn_client_node/RigidBody2/pose", PoseStamped, self.record_data_callback2)
-        while True:
-            if self.CalipenCalibrationData_num_of_data_tocka1 > self.N_num_of_data_tocka:
-                #self.save_list_to_csv(self.CalipenCalibrationData_list_tocka1, self.csv_filename)
-                callipenpose_subscriber2.unregister()
-                break
-            
-    def RecordData_tocka2(self):
-        callipenpose_subscriber3 = rospy.Subscriber("/vrpn_client_node/RigidBody2/pose", PoseStamped, self.record_data_callback3)
-        while True:
-            if self.CalipenCalibrationData_num_of_data_tocka2 > self.N_num_of_data_tocka:
-                #self.save_list_to_csv(self.CalipenCalibrationData_list_tocka2, self.csv_filename)
-                callipenpose_subscriber3.unregister()
-                break
-                    
+
         
-        #print(self.CalipenCalibrationData_list)
 
 
     def CalculateCalipenTransformation(self):
@@ -220,10 +136,70 @@ class CallipenCalibration:
                 break
 
 
+def pose_to_T(pose):
 
+    pose = data.pose
+    q1 = pose.orientation.x
+    q2 = pose.orientation.y
+    q3 = pose.orientation.z
+    q4 = pose.orientation.w
+    x = pose.position.x
+    y = pose.position.y
+    z = pose.position.z
+    R = quat2mat([q1, q2, q3, q4])
+    
+    T = np.zeros([4, 4])
+    T[:3, :3] = R
+    T[0, 3] = x
+    T[1, 3] = y
+    T[2, 3] = z
+    T[3, 3] = 1
+    return T
 
+def objectiveFunPosition(P, R, T_data):
+    """
+    Optimization for position
+    """
+    T0A = T_data.copy()
+    Tx = np.eye(4)
+    Tx[:3, :3] = R
+    Tx[:3, 3] = P
 
+    num_of_mesurs = len(T0A)  # holds the number of measurements
+    f = 0  # optimization function value initialized to 0
+    print(num_of_mesurs)
 
+    for i in range(0, num_of_mesurs):
+        T0o1_old = T0A[i] @ Tx
+        for j in range(0, num_of_mesurs):
+            T0o1 = T0A[j] @ Tx
+            p = T0o1[:3, 3] - T0o1_old[:3, 3]
+            #print(p)
+            f += np.linalg.norm(p)  # Euclidean distance
+    print(f)
+    return f
+
+def f_optimizePointOrientation(T_init, callibration_data_poses):
+
+    
+    R1 = T_init[:3, :3]  # Initial rotation matrix
+    P1 = T_init[:3, 3]   # Initial position vector
+
+    # Optimize position using fmin
+    P1_optimized = fmin(func=self.objectiveFunPosition, x0=P1, args=(R1, callibration_data_poses), xtol=1e-4, ftol=1e-4,  disp=True)
+
+    print("optimization finished")
+    print(P1_optimized)
+    T = np.eye(4)
+    T[:3, 3] = P1_optimized
+
+    return T
+
+def CalculateCalipenTransformation(callibration_data_poses):
+        T_init = np.eye(4, 4)
+        
+        T = f_optimizePointOrientation(T_init, callibration_data_poses)
+        return T
 
 if __name__ == '__main__':
     try:
